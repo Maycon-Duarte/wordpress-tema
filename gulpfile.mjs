c0efa216e6805d001f3802b288998b400045348b
Maycon Duartenpm run scss  git config --global user.email mgam2004@gmail.com  git config --global user.name Maycon <mgam2004@gmail.com>
2024-11-02 16:23:46 -0300

    :ok_hand: atualizando dependencias

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,13 +4,16 @@ import gulpSass from 'gulp-sass';
 import include from 'gulp-include';
 import npmMainfiles from 'gulp-npm-mainfiles';
 import zip from 'gulp-zip';
+import browserSync from 'browser-sync';
 
+const bs = browserSync.create();
 const gulpSassCompiler = gulpSass(sass);
 
 gulp.task('sass', function () {
     return gulp.src('assets/scss/**/*.scss')
         .pipe(gulpSassCompiler().on('error', gulpSassCompiler.logError))
-        .pipe(gulp.dest('assets/css'));
+        .pipe(gulp.dest('assets/css'))
+        .pipe(bs.stream());
 });
 
 gulp.task('watch', function () {
@@ -24,6 +27,18 @@ gulp.task('copy-dependencies', function (done) {
         .pipe(gulp.dest('./assets/lib'))
         .on('end', done);  // Adicionando o callback done para indicar conclusão assíncrona
 });
+
+gulp.task('serve', function () {
+    bs.init({
+        proxy: "solicitacoes-taesa.local", // Configurando proxy para o servidor PHP
+        notify: false
+    });
+
+    gulp.series('watch')();
+
+    gulp.watch('**/*.php').on('change', bs.reload);
+});
+
 
 gulp.task('zip', function () {
     return gulp.src([
@@ -55,7 +70,8 @@ gulp.task('scripts', function () {
     return gulp.src('assets/js/scripts.js')
         .pipe(include())
         .on('error', console.log)
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./'))
+        .pipe(bs.stream());
 });
 
 gulp.task('default', gulp.series('copy-dependencies', 'sass', 'scripts', 'watch'));
